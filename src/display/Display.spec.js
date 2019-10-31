3bb30383e2b83632a1c9e547e6fb675259a655f6
Ali Dervisoglu <derya.dervisoglu@gmail.com>
2019-10-30 18:17:33 -0700

    MVP met

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,20 @@
 // Test away!
+
+import React from "react"
+import { render } from "react-testing-library"
+import Display from "./Display";
+
+describe("display", () => {
+
+    it("gate is unlocked and open) ", () => {
+      const { getByText } = render(<Display />);
+      expect(getByText("Unlocked"));
+      expect(getByText("Open"));
+    })
+
+    it("gate is locked and closed", () => {
+      const { getByText } = render(<Display locked={true} closed={true} />);
+      expect(getByText("Locked"));
+      expect(getByText("Closed"));
+    })
+  })
